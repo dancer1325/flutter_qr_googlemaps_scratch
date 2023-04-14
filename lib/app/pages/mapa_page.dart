be43797e76a3425fee17c7f434dc00f795cc2066
dancer1325 <alfredotic0809@gmail.com>
2023-04-14 19:06:34 +0200

    doc(Section10): add commentaries about google_maps_flutter functionalities

--- lib/app/pages/mapa_page.dart
+++ lib/app/pages/mapa_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_qr_googlemaps_scratch/app/models/scan_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+// Required to extend from StatefulWidget for using google_maps_flutter
+// https://pub.dev/packages/google_maps_flutter#both
 class MapaPage extends StatefulWidget {
 
   @override
@@ -12,7 +14,12 @@ class MapaPage extends StatefulWidget {
 
 class _MapaPageState extends State<MapaPage> {
 
+  // Control the map view
+  // https://pub.dev/documentation/google_maps_flutter/latest/google_maps_flutter/GoogleMapController-class.html
   Completer<GoogleMapController> _controller = Completer();
+
+  // https://pub.dev/documentation/google_maps_flutter_platform_interface/latest/google_maps_flutter_platform_interface/MapType.html
+  // Property whose changes can be tracked
   MapType mapType = MapType.normal;
 
   @override
@@ -21,13 +28,14 @@ class _MapaPageState extends State<MapaPage> {
     // Read the arguments passed
     final scan = ModalRoute.of(context)?.settings.arguments as ScanModel;
 
-    final puntoInicial = CameraPosition(
+    final initialPoint = CameraPosition(
       target: scan.getLatLng(),
       zoom: 17.5,
       tilt: 50
     );
 
     // Markers
+    // https://pub.dev/documentation/google_maps_flutter_platform_interface/latest/google_maps_flutter_platform_interface/Marker-class.html
     var markers = new Set<Marker>();
     markers.add(new Marker(
       markerId: MarkerId('geo-location'),
@@ -46,7 +54,7 @@ class _MapaPageState extends State<MapaPage> {
               controller.animateCamera(
                 CameraUpdate.newCameraPosition(
                   CameraPosition(
-                    target: scan.getLatLng(),
+                    target: scan.getLatLng(),   // Get easier come back to the previous position
                     zoom: 17.5,
                     tilt: 50
                   )
@@ -57,10 +65,11 @@ class _MapaPageState extends State<MapaPage> {
         ],
       ),
       body: GoogleMap(
+        // https://pub.dev/documentation/google_maps_flutter/latest/google_maps_flutter/GoogleMap-class.html
         myLocationButtonEnabled: false,
         mapType: mapType,
         markers: markers,
-        initialCameraPosition: puntoInicial,
+        initialCameraPosition: initialPoint,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
@@ -68,6 +77,7 @@ class _MapaPageState extends State<MapaPage> {
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.layers ),
         onPressed: () {
+          // Be able to switch mapType
 
           if ( mapType == MapType.normal ) {
             mapType = MapType.satellite;
@@ -75,6 +85,7 @@ class _MapaPageState extends State<MapaPage> {
             mapType = MapType.normal;
           }
 
+          // Function to redraw the StatefulWidget with state mapType
           setState(() {});
         }
       ),
